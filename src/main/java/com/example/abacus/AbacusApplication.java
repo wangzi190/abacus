67b866346f12e39f5f8aece0c67e7183d3f42539
soren <57303754+wangzi190@users.noreply.github.com>
2022-11-07 10:49:47 -0800

    Add files via upload

---
+++ src/main/java/com/example/abacus/AbacusApplication.java
@@ -0,0 +1,13 @@
+package com.example.abacus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/abacus/AbacusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abacus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbacusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
